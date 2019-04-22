33ea23c265232e6039ca1c92bcd5fbfa229b3fe3
Riccardo H <Ace_Graphics@outlook.de>
2019-04-22 02:39:23 +0200

    added example dir to config

--- config.bat
+++ config.bat
@@ -1,3 +1,6 @@
 :: Enter your directories here
 set cssDir=""
 set momDir=""
+:: Example
+:: set cssDir="C:\Steam\steamapps\common\counter-strike source"
+:: set momDir="C:\Steam\steamapps\sourcemods\momentum"
